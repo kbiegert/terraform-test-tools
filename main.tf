56fe88a29c42cae7909fff4ea6ff9b01fb44ff1e
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-02-15 12:43:47 -0500

    query an offering from a catalog

--- main.tf
+++ main.tf
@@ -7,10 +7,23 @@ data "ibm_cm_catalog" "cm_catalog" {
 }
 
 # this will create a catalog with the given name and description
-resource "ibm_cm_catalog" "cm_catalog" {
-  label = "kb-test-catalog"
-  short_description = "created via terraform"
+# resource "ibm_cm_catalog" "cm_catalog" {
+#   label = "kb-test-catalog"
+#   short_description = "created via terraform"
+# }
+
+# read the catalog data source and retrieve the catalog id for use here.
+data "ibm_cm_offering" "cm_offering" {
+    catalog_id = data.ibm_cm_catalog.cm_catalog.id
+    offering_id = "9f98f576-d9fa-4dfc-8467-1042024ecd38"
 }
+
+# this will create an offering with the given name in the given catalog
+# resource "ibm_cm_offering" "cm_offering" {
+#   catalog_id = "b92d6984-f42a-4ba8-bf3a-2bcdb132538c"
+#   label = "test offering"
+#   long_description = "this is a long description of this offering"
+# }
 
 # data "ibm_cm_version" "cm_version" {
 #     version_loc_id = "b92d6984-f42a-4ba8-bf3a-2bcdb132538c.1c1186a9-ec67-44d8-abbc-9a545a265751"
@@ -28,3 +41,4 @@ resource "ibm_cm_catalog" "cm_catalog" {
 #   }
 #   mark_version_consumable = true
 # }
+
--- output.tf
+++ output.tf
@@ -1,4 +1,8 @@
-output "version-info" {
+output "catalog-info" {
     # value = data.ibm_cm_version.cm_version.version_loc_id
-    value = data.ibm_cm_catalog.cm_catalog.created
+    value = data.ibm_cm_catalog.cm_catalog
+}
+
+output "offering-info" {
+    value = data.ibm_cm_offering.cm_offering
 }
