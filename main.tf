6b6de6e977f40ea0002d7a8670f81b474a2920df
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-02-24 15:36:35 -0500

    utilize beta tf provider

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 # 
 
 # read only data source - read an existing catalog 
+# catalog "my-GoldenEye-offerings" in prod under keith biegert account
 data "ibm_cm_catalog" "cm_catalog" {
     catalog_identifier = "b92d6984-f42a-4ba8-bf3a-2bcdb132538c"
 }
@@ -13,9 +14,17 @@ data "ibm_cm_catalog" "cm_catalog" {
 # }
 
 # read the catalog data source and retrieve the catalog id for use here.
-data "ibm_cm_offering" "cm_offering" {
+# offering "custom-apache" in catalog "my-GoldenEye-offerings"
+data "ibm_cm_offering" "cm_offering1" {
     catalog_id = data.ibm_cm_catalog.cm_catalog.id
     offering_id = "9f98f576-d9fa-4dfc-8467-1042024ecd38"
+}
+
+# read the catalog data source and retrieve the catalog id for use here.
+# offering "terraform-sample-1.1.0" in catalog "my-GoldenEye-offerings"
+data "ibm_cm_offering" "cm_offering2" {
+    catalog_id = data.ibm_cm_catalog.cm_catalog.id
+    offering_id = "be7eb954-bba3-4fe8-9eb6-ab09743adcd9"
 }
 
 # this will create an offering with the given name in the given catalog
@@ -25,10 +34,15 @@ data "ibm_cm_offering" "cm_offering" {
 #   long_description = "this is a long description of this offering"
 # }
 
-# data "ibm_cm_version" "cm_version" {
-#     version_loc_id = "b92d6984-f42a-4ba8-bf3a-2bcdb132538c.1c1186a9-ec67-44d8-abbc-9a545a265751"
-# }
+# version 0.0.24 of offering "custom-apache" in catalog "my-GoldenEye-offerings"
+data "ibm_cm_version" "cm_version1" {
+    version_loc_id = "b92d6984-f42a-4ba8-bf3a-2bcdb132538c.1c1186a9-ec67-44d8-abbc-9a545a265751"
+}
 
+# version 1.1.0 of offering "terraform-sample-1.1.0" in catalog "my-GoldenEye-offerings"
+data "ibm_cm_version" "cm_version2" {
+    version_loc_id = "b92d6984-f42a-4ba8-bf3a-2bcdb132538c.60b9e398-a208-4021-8a1e-d85a168c9db7"
+}
 #data "ibm_iam_auth_token" "tokendata" {}
 
 # resource "ibm_cm_validation" "my_cm_version_validation" {
@@ -41,4 +55,3 @@ data "ibm_cm_offering" "cm_offering" {
 #   }
 #   mark_version_consumable = true
 # }
-
--- output.tf
+++ output.tf
@@ -3,6 +3,17 @@ output "catalog-info" {
     value = data.ibm_cm_catalog.cm_catalog
 }
 
-output "offering-info" {
-    value = data.ibm_cm_offering.cm_offering
+output "offering-info1" {
+    value = data.ibm_cm_offering.cm_offering1
+}
+
+output "offering-info2" {
+    value = data.ibm_cm_offering.cm_offering2
+}
+
+output "version-info1" {
+    value = data.ibm_cm_version.cm_version1
+}
+output "version-info2" {
+    value = data.ibm_cm_version.cm_version2
 }
